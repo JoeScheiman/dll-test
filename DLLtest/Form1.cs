14ed418b7cf06a86cd390287668e89a364723661
Joe Scheiman <scheiman.joe@gmail.com>
2019-05-14 13:18:18 -0400

    first commit

---
+++ DLLtest/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculateLibraryJS;
+
+namespace DLLtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Calculate cal = new Calculate();
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = cal.Add(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                textBox3.Text = i.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Check your Nums");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = cal.Sub(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                textBox3.Text = i.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Check your Nums");
+            }
+
+        }
+    }
+}
